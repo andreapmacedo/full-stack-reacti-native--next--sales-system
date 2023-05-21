16bb84b1f4c6a682adc82465930e1c0d58b12fef
andreapmacedo <aa.andremacedo@gmail.com>
2023-05-20 22:39:42 -0300

    Feat: Adiciona store para redux.

--- frontend/src/modules/login/hooks/useLogin.ts
+++ frontend/src/modules/login/hooks/useLogin.ts
@@ -1,15 +1,23 @@
 import { useState } from 'react';
 import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native';
+import { useSelector } from 'react-redux';
+
 import { useRequest } from '../../../shared/hooks/useRequest';
+import { RootState } from '../../../store';
 
 
 
 export const useLogin = () => {
+  const  user  = useSelector((state: RootState) => state.userReducer.user);
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   
   const {authRequest, errorMessage, loading, setErrorMessage} = useRequest();
   
+
+  console.log(user);
+  
+
   const handleOnPress = async () => {
     authRequest({ email, password });
   };
