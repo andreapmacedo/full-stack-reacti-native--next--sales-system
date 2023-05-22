d08abb0cd0f1afa1e94af663a172f15aa54954eb
andreapmacedo <aa.andremacedo@gmail.com>
2023-05-22 20:24:37 -0300

    Feat: Adiciona inset user para redux.

--- frontend/src/modules/login/hooks/useLogin.ts
+++ frontend/src/modules/login/hooks/useLogin.ts
@@ -4,18 +4,20 @@ import { useSelector } from 'react-redux';
 
 import { useRequest } from '../../../shared/hooks/useRequest';
 import { RootState } from '../../../store';
+import { useUserReducer } from '../../../store/reducers/userReducer/useUserReducer';
 
 
 
 export const useLogin = () => {
-  const  user  = useSelector((state: RootState) => state.userReducer.user);
+  // const  user  = useSelector((state: RootState) => state.userReducer.user);
+  const  { user }  = useUserReducer();
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   
   const {authRequest, errorMessage, loading, setErrorMessage} = useRequest();
   
 
-  console.log(user);
+  console.log('user', user);
   
 
   const handleOnPress = async () => {
--- frontend/src/store/reducers/userReducer/useUserReducer.ts
+++ frontend/src/store/reducers/userReducer/useUserReducer.ts
@@ -1,19 +1,19 @@
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-// import { UserType } from '../../../shared/types/userType';
-// import { useAppSelector } from '../../hooks';
-// import { setUserAction } from '.';
+import { UserType } from '../../../shared/types/userType';
+import { useAppSelector } from '../../hooks';
+import { setUserAction } from '.';
 
-// export const useUserReducer = () => {
-//   const dispatch = useDispatch();
-//   const { user } = useAppSelector((state) => state.userReducer);
+export const useUserReducer = () => {
+  const dispatch = useDispatch();
+  const { user } = useAppSelector((state) => state.userReducer);
 
-//   const setUser = (currentUser: UserType) => {
-//     dispatch(setUserAction(currentUser));
-//   };
+  const setUser = (currentUser: UserType) => {
+    dispatch(setUserAction(currentUser));
+  };
 
-//   return {
-//     user,
-//     setUser,
-//   };
-// };
+  return {
+    user,
+    setUser,
+  };
+};
