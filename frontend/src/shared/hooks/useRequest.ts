26f120ee35c633955de1106b7bfcc8f33e69c84b
andreapmacedo <aa.andremacedo@gmail.com>
2023-05-28 08:24:20 -0300

    Adiciona aula 61.

--- frontend/src/shared/hooks/useRequest.ts
+++ frontend/src/shared/hooks/useRequest.ts
@@ -5,68 +5,24 @@ import { RequestLogin } from './../types/requestLogin';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useUserReducer } from '../../store/reducers/userReducer/useUserReducer';
+import { setUserAction } from '../../store/reducers/userReducer';
 
 
-
-// interface requestProps<T, B = unknown> {
-//   url: string;
-//   method: MethodType;
-//   saveGlobal?: (object: T) => void;
-//   body?: B;
-//   message?: string;
-// }
 
 export const useRequest = () => {
-  // const { reset } = useNavigation<NavigationProp<ParamListBase>>();
-  const { setUser } = useUserReducer();
-  // const { setModal } = useGlobalReducer();
   const [loading, setLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>('');
-  // const [user, setUser] = useState<UserType>();
+    
+  
   const dispatch = useDispatch();
 
-  // const request = async <T, B = unknown>({
-  //   url,
-  //   method,
-  //   saveGlobal,
-  //   body,
-  //   message,
-  // }: requestProps<T, B>): Promise<T | undefined> => {
-  //   setLoading(true);
-  //   const returnObject: T | undefined = await ConnectionAPI.connect<T, B>(url, method, body)
-  //     .then((result) => {
-  //       if (saveGlobal) {
-  //         saveGlobal(result);
-  //       }
-  //       if (message) {
-  //         setModal({
-  //           visible: true,
-  //           title: 'Sucesso!',
-  //           text: message,
-  //         });
-  //       }
-  //       return result;
-  //     })
-  //     .catch((error: Error) => {
-  //       setModal({
-  //         visible: true,
-  //         title: 'Erro',
-  //         text: error.message,
-  //       });
-  //       return undefined;
-  //     });
-
-  //   setLoading(false);
-  //   return returnObject;
-  // };
 
   const authRequest = async (body: RequestLogin) => {
     setLoading(true);
     const x = await connectionAPIPost<ReturnLogin>('http://192.168.0.15:8080/auth', body)
       .then((result) => {
+        dispatch(setUserAction(result.user));
         // setUser(result.user);
-        // dispatch(setUserAction(result.user));
-        setUser(result.user);
       })
       .catch(() => {
       console.log('erro');
