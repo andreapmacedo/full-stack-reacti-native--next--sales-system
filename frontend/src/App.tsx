1032e486801306a967b26eb7f19fd26c0daa423f
andreapmacedo <aa.andremacedo@gmail.com>
2023-05-28 14:39:54 -0300

    Adiciona aula 65.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,10 @@
 import { SafeAreaView } from "react-native";
 import styled from 'styled-components/native';
-import Login from "./modules/login";
 import { useState } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import GlobalModal from "./shared/components/modal/GlobalModal/GlobalModal";
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from "./modules/home/screens/Home";
-
-const Stack = createNativeStackNavigator();
-
+import Navigation from "./Navigation";
 
 export const NewText = styled.Text`
   font-size: 20px;
@@ -18,21 +12,12 @@ export const NewText = styled.Text`
 `;
 
 const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
+  // const [modalVisible, setModalVisible] = useState(false);
   return (
     <Provider store={store}>
       <GlobalModal />
       {/* <SafeAreaView> */}
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Login"
-            component={Login}
-          />
-            <Stack.Screen name="Home" component={Home} options={{title: 'Home'}} />
-          </Stack.Navigator>
-        </NavigationContainer>
-        {/* <Login /> */}
+        <Navigation />
       {/* </SafeAreaView> */}
     </Provider>
   );
